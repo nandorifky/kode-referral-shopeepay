a0c4b6510dda5ee974972bf9793e63bc0b9cc48a
Nando Rifky <mr.winando@gmail.com>
2025-12-14 01:41:34 +0700

    asda

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog ShopeePay Referral';
+export const SITE_DESCRIPTION = 'Panduan lengkap penggunaan ShopeePay dan berbagai tips menarik seputar referral ShopeePay.';
